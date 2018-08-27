a5a5e6bf0f87d96fceb1a5198ac2a9f3f167c878
Ian Danforth <iandanforth@gmail.com>
2018-08-27 12:15:55 -0700

    Initial solve using mostly unmodified solution code

--- freeplay.py
+++ freeplay.py
@@ -1,4 +1,5 @@
 import msvcrt  # Windows only
+import numpy as np
 from unityagents import UnityEnvironment
 
 """
@@ -22,6 +23,7 @@ By default the agent will always move forward unless you press a key.
     s - move backward
     d - turn right
 """
+DEBUG = False
 
 
 def kbfunc():
@@ -38,6 +40,31 @@ def kbfunc():
     return ret
 
 
+def print_state(state):
+    rays = state[:-2]
+    rays = rays.reshape(7, 5)
+    r_20, r_90, r_160, r_45, r_135, r_70, r_110 = rays
+    print("Banana Wall BadBanana Agent Distance")
+    rays = np.array([
+        r_20,
+        r_45,
+        r_70,
+        r_90,
+        r_110,
+        r_135,
+        r_160]
+    )
+    print(rays)
+    velocity = state[-2:]
+    print(velocity)
+
+def print_ray(episode, score, duration, newline=False):
+    end = None if newline else ""
+    print(
+        '\rEpisode {}\tAverage Score: {:.2f}\t Duration: {:.2f}'.format(episode, score, duration),
+        end=end
+    )
+
 def main():
     env = UnityEnvironment(file_name="unity/Banana_Windows_x86_64/Banana.exe")
 
@@ -49,7 +76,7 @@ def main():
     env_info = env.reset(train_mode=True)[brain_name]
 
     # Debug helpers
-    if False:
+    if DEBUG:
         # number of actions
         action_size = brain.vector_action_space_size
         print('Number of actions:', action_size)
@@ -65,6 +92,7 @@ def main():
 
     score = 0
     counter = 0
+    np.set_printoptions(precision=3, suppress=True)
     while True:
         # Check for keystrokes
         x = kbfunc()
@@ -80,11 +108,12 @@ def main():
                 action = 1
         else:
             # By default move forward
-            action = 0
+            action = -1
 
         counter += 1
         env_info = env.step(action)[brain_name]        # send the action to the environment
         next_state = env_info.vector_observations[0]   # get the next state
+        if DEBUG: print_state(next_state)
         reward = env_info.rewards[0]                   # get the reward
         done = env_info.local_done[0]                  # see if episode has finished
         score += reward                                # update the score
--- train.py
+++ train.py
@@ -1,12 +1,21 @@
 """
 Solution Code from Udacity Deep Reinforcement Learning Course
 """
-import numpy as np
+import time
 import torch
+import numpy as np
 import matplotlib as plt
 from collections import deque
 from dqn_agent import Agent
 from unityagents import UnityEnvironment
+
+
+def print_update(episode, score, duration, newline=False):
+    end = None if newline else ""
+    print(
+        '\rEpisode {}\tAverage Score: {:.2f}\t Duration: {:.2f}'.format(episode, score, duration),
+        end=end
+    )
 
 
 def dqn(env, agent, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
@@ -26,8 +35,10 @@ def dqn(env, agent, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, ep
     for i_episode in range(1, n_episodes + 1):
         state = env.reset()
         score = 0
+        e_start = time.time()
         for t in range(max_t):
             action = agent.act(state, eps)
+            action = int(action)
             next_state, reward, done, _ = env.step(action)
             agent.step(state, action, reward, next_state, done)
             state = next_state
@@ -37,11 +48,13 @@ def dqn(env, agent, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, ep
         scores_window.append(score)        # save most recent score
         scores.append(score)               # save most recent score
         eps = max(eps_end, eps_decay*eps)  # decrease epsilon
-        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        mean_score = np.mean(scores_window)
+        e_duration = time.time() - e_start
+        print_update(i_episode, mean_score, e_duration)
         if i_episode % 100 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-        if np.mean(scores_window) >= 200.0:
-            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            print_update(i_episode, mean_score, e_duration, newline=True)
+        if mean_score >= 13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, mean_score))
             torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
             break
     return scores
@@ -95,7 +108,7 @@ def main():
     #########################
     # Environment
 
-    unity_env = UnityEnvironment(file_name="unity/Banana_Windows_x86_64/Banana.exe")
+    unity_env = UnityEnvironment(file_name="unity/Banana_Windows_x86_64/Banana.exe", no_graphics=True)
     env = SimpleGymWrapper(unity_env)
 
     ########################
